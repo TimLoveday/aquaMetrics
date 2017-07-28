c71e52aeab872b6017032b971009e471619dc359
Foster <Tim.Foster@SEPA.org.uk>
2017-07-28 11:16:50 +0100

    fix: rounding issue in test

--- tests/testthat/test-CalcDARLEQ.R
+++ tests/testthat/test-CalcDARLEQ.R
@@ -14,18 +14,41 @@ test_that("CalcDARLEQ returns correct error measures", {
 
 })
 
-test_that("CalcDARLEQ returns same results as DARLEQ outputs", {
+test_that("CalcDARLEQ returns same results as DARLEQ2 tool outputs", {
 
-riverAquaMetrics  <-  read.csv(system.file("extdata","diatomRivers2012-aquametrics-format.csv",
-                               package = "aquaMetrics"))
+    # load example data
+    riverAquaMetrics  <-  read.csv(system.file("extdata","diatomRivers2012-aquametrics-format.csv",
+                                   package = "aquaMetrics"))
+    # load matching outputs from DARLEQ2 - 'one prepared earlier'
+    riverDARLEQRResults  <-  read.csv(system.file("extdata","ResultsDARLEQRiver2012.csv",
+                                     package = "aquaMetrics"))
 
-riverDARLEQRResults  <-  read.csv(system.file("extdata","ResultsDARLEQRiver2012.csv",
-                                 package = "aquaMetrics"))
+    # run example data through CalcDARLEQ function
+    riverAquaMetricsResults  <-  CalcDARLEQ(riverAquaMetrics, metric="rivers")
+    riverAquaMetricsResults  <- data.frame(riverAquaMetricsResults[1])
 
-riverAquaMetricsResults  <-  CalcDARLEQ(riverAquaMetrics, metric="rivers")
-riverAquaMetricsResults  <- data.frame(riverAquaMetricsResults[1])
-expect_equal(round(riverAquaMetricsResults$rivers.oTDI4,2), round(riverDARLEQRResults$TDI4,2))
+    # need to round result like Excel
+    round_excel = function(x, n) {
+      posneg = sign(x)
+      z = abs(x)*10^n
+      z = z + 0.5
+      z = trunc(z)
+      z = z/10^n
+      z*posneg
+    }
 
-expect_equal(round(riverAquaMetricsResults$rivers.EQR.TDI4,2), round(riverDARLEQRResults$EQR.TDI4,2))
+    # hack to get rid of decimal place issues:
+    # '...since e.g. 0.15 is not represented exactly, the rounding rule applies to the
+    # represented number and not to the printed number, and so round(0.15, 1) could
+    # be either 0.1 or 0.2).'
+    riverAquaMetricsResults$rivers.oTDI4 <- as.character(riverAquaMetricsResults$rivers.oTDI4)
+    riverAquaMetricsResults$rivers.oTDI4 <- as.numeric(riverAquaMetricsResults$rivers.oTDI4)
+
+    # check CalcDARLEQ outputs match DARLEQ2 tool outputs
+    expect_equal(round_excel(riverAquaMetricsResults$rivers.oTDI4,2), round_excel(riverDARLEQRResults$TDI4,2))
+    expect_equal(round_excel(riverAquaMetricsResults$rivers.EQR.TDI4,2), round_excel(riverDARLEQRResults$EQR.TDI4,2))
+
 
 })
+
+
